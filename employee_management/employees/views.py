bcbac1d50cbe07aa93e63670cd8ef335eaf4a843
Gaurav <gaurav.kaushal@nlineaxis.com>
2024-11-14 18:04:12 +0530

    Commit to Add New Folder for Template & Static (css & JS) along with the updated index.html page.

--- employee_management/employees/views.py
+++ employee_management/employees/views.py
@@ -6,3 +6,5 @@ def employee_list(request):
     employees = Employee.objects.all()
     return render(request, 'employees/employee_list.html', {'employees': employees})
 
+def index(request):
+    return render(request, 'index.html')
---
+++ employee_management/static/js/scripts.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    alert("Welcome to the Employee Management System! Here, you can manage your employee details easily.");
+}
